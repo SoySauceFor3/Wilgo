1624d496eee64ea91cfd7d03852155ed9371c6ab
SoySauceFor3 <xinyayang0506@gmail.com>
2026-02-24 03:17:18 +0800

    Initial Commit

---
+++ WilgoTests/WilgoTests.swift
@@ -0,0 +1,16 @@
+//
+//  WilgoTests.swift
+//  WilgoTests
+//
+//  Created by Xinya Yang on 2/24/26.
+//
+
+import Testing
+
+struct WilgoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
